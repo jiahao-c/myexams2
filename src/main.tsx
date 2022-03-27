bbdb3296b73c4e926e815857367e9c9d65aeec7d
Jiahao <jahowchen@outlook.com>
2022-03-27 11:14:35 -0400

    add locale

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "virtual:windi.css";
 import { Home } from "./pages/Home";
+import { ConfigProvider } from '@arco-design/web-react';
+import enUS from '@arco-design/web-react/es/locale/en-US';
 
 ReactDOM.render(
-  <Home />,
+  <ConfigProvider locale={enUS}>
+  <Home />
+  </ConfigProvider>
+  ,
   document.getElementById("root"),
 );
